20e2ca6b74887f3bbe42353149ed88cbc13cacb3
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2022-09-22 00:32:06 +0700

    Initial Commit

---
+++ littleCV/littleCVApp.swift
@@ -0,0 +1,17 @@
+//
+//  littleCVApp.swift
+//  littleCV
+//
+//  Created by Matthias Kreier on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct littleCVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
